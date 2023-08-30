4a3d809db27196bf8c7073c4c80fdc4b18d4bc74
vladlisovkyi <vlad.lisovskyi.dev@gmail.com>
2023-08-30 12:12:05 +0300

    first commit

---
+++ components/Feed.js
@@ -0,0 +1,30 @@
+import { useRecoilState } from "recoil";
+import { userState } from "../atom/userAtom";
+import MiniProfile from "./MiniProfile";
+import Posts from "./Posts";
+import Stories from "./Stories";
+import Suggestions from "./Suggestions";
+
+export default function Feed() {
+  const [currentUser] = useRecoilState(userState);
+  return (
+    <main
+      className={`grid ${
+        currentUser
+          ? "grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto"
+          : "grid-cols-1 md:grid-cols-2 md:max-w-3xl mx-auto"
+      }  `}
+    >
+      <section className="md:col-span-2">
+        <Stories />
+        <Posts />
+      </section>
+      <section className="hidden md:inline-grid md:col-span-1">
+        <div className="fixed w-[380px]">
+          <MiniProfile />
+          <Suggestions />
+        </div>
+      </section>
+    </main>
+  );
+}
